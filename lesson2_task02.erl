7676a3de33757390c3059beba261671bf91cb6fd
Arthur Sivkov <80418177+CobbiFantom@users.noreply.github.com>
2023-09-04 09:11:35 +0300

    Update lesson2_task02.erl

--- lesson2_task02.erl
+++ lesson2_task02.erl
@@ -1,7 +1,12 @@
--module(lesson2_task02).
+-module(test).
 -export([but_last/1]).
+
 
 but_last(L) -> but_last(L, [], []).
 
-but_last([],A,B) -> [A,B];
-but_last([H|T],_,B) -> but_last(T,B,H).
+but_last([],A,B) when A =/= [] -> 
+  [A,B];
+but_last([H|T],_,B) -> 
+  but_last(T,B,H);
+but_last(L,[],_) ->
+  wrong_arguments.
